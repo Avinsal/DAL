a1acd0145a7294c471691b3f18c841eb7b27c71e
Avinsal <113495656+Avinsal@users.noreply.github.com>
2023-02-10 01:31:34 +0530

    Add files via upload

--- Q2.c
+++ Q2.c
@@ -55,15 +55,15 @@ int main()
     pn=Primefactorn(n);
     int* com=(int*)malloc(1024*sizeof(int));
     int z=0;
-    for(int i=0;i<20;i++)
+    for(int i=0;i<10;i++)
     {
-        for(int j=0;j<20;j++)
+        for(int j=1;j<10;j++)
         {   
-            if(pn[i]==pm[j])
+            if(pm[i]==pn[j])
             {           
-                z++;
-                com[i]=pn[i]=pm[j];
-                i++;           
+                  z++;
+                  com[i]=pm[i]=pn[j];
+                  i++;
             }
         }
     }
@@ -72,19 +72,30 @@ int main()
         printf("%d",com[i]);
     }
     int cn=0;
-    for(int i=0;i<1024;i++)
+    for(int i=0;i<z;i++)
     {
         if(com[i]!=0)
         {
             cn++;
         }
     }
-    printf("%d",cn);
+    printf("\ncn=%d",cn);
     if(cn==0)
     {
         printf("GCD is 1");
     }
+    int gcd=1;
+    for(int i=0;i<z;i++)
+    {
+        if(com[i]!=0)
+        {
+            gcd=gcd*com[i];
+        }
+    }
+    printf("\nGCD is %d",gcd);
+    return 0;
 
     
   
+    
 }
